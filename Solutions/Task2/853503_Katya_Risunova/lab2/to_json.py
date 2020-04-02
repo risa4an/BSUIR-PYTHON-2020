b6edf7c78aa5350a1cd46c81e99b06a0bd8baea8
risa4an <48015516+risa4an@users.noreply.github.com>
2020-04-02 10:22:57 +0300

    Update to_json.py

--- Solutions/Task2/853503_Katya_Risunova/lab2/to_json.py
+++ Solutions/Task2/853503_Katya_Risunova/lab2/to_json.py
@@ -14,32 +14,84 @@ class Book(object):
             self.pages = 346
         Book.__instance = self
 
-
-def is_float(value):
-    try:
-        float(value)
-        return True
-    except ValueError:
-        return False
-
-
 def to_json(obj):
-    json = "{"
-    for key, value in obj.__dict__.items():
-        json += "\"" + str(key) + "\": " + ((str(value)) if is_float(value) else ("\"" + str(value) + "\"")) + ", "
-    json = json[:-2]
-    json += "}"
+    json = '{ \n'
+    cl = str(obj.__class__)
+    if isinstance(obj,(int,float)):
+        json += get_name(obj) + " :" + str(obj) + ',\n'
+    elif isinstance(obj,bool):
+        json += get_name(obj) + " :" + str(obj).lower() + ',\n'
+    elif isinstance (obj, str):
+        json += get_name(obj) +  " :\"" + obj + '\",\n'
+    elif isinstance(obj, (list,tuple)):
+        json += get_name(obj) + " :[\n"
+        for v in obj:
+            json += to_json_noname(v)
+        json = json [:-2] + '\n]\n'
+    elif isinstance(obj, dict):
+        json += get_name(obj) + " :{\n"
+        for k,v in obj.items():
+            json += "\"" + str(k) + "\" :" + to_json_noname(v)
+        json = json[:-2] + '\n}\n'
+    elif "<class \'__main__." in cl:
+        json += get_name(obj) + " :{\n"
+        for k, v in obj.__dict__.items():
+            json += "\"" + str(k) + "\" :" + to_json_noname(v)
+        json = json[:-2] + '\n}\n'
+    if json[len(json) - 2]==',':
+        json = json[:-2] + '\n}\n'
+    else:
+        json += "}\n"
     return json
+
+def to_json_noname(obj):
+    json = ''
+    if isinstance(obj, (int,float)):
+        json += str(obj) + ',\n'
+    elif isinstance(obj,str):
+        json += "\"" + obj + "\",\n "
+    elif isinstance(obj, bool):
+        json += str(obj).lower() + ",\n"
+    elif isinstance(obj, (list, tuple)):
+        json += '[\n'
+        for v in obj:
+            json += to_json_noname(v)
+        json =json[:-2] + '\n],\n'
+    elif isinstance(obj, dict):
+        json += "{\n"
+        for k,v in obj.items():
+            json += "\"" + str(k) + "\" :" + to_json_noname(v)
+        json = json[:-2] +  '\n},\n'
+    elif "<class \'__main__." in str(obj.__class__):
+        json += '{\n'
+        for k, v in obj.__dict__.items():
+            json += "\"" + str(k) + "\" :" + to_json_noname(v)
+        json = json[:-2] + "\n},\n"
+    return json
+
+def get_name(obj):
+    for k, v in globals().items():
+        idi = id(v)
+        idobj = id(obj)
+        if idi == idobj:
+            if k == None:
+                return " "
+            else:
+                return "\"" + k + "\" "
 
 
 if __name__ == "__main__":
     book = Book()
-
     book = Book.get_instance()
-    print(book)
-    print(to_json(book))
-
+    print (to_json(book))
+    m = "hi!"
+    print(to_json(m))
+    k = [1,2,3]
+    d = {'dict': 1, 'dictionary': 2}
+    print (to_json(k))
     murakami = Book()
+    murakami.author = "H.Murakami"
     murakami.name = "Killing comrade"
-    murakami.pages = "143"
-    print(to_json(murakami))
+    murakami.pages = 143
+    k = [book, murakami]
+    print(to_json(k))
